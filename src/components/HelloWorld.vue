ba55737e3bba4ef0319d81fa13d351fdfa2b8131
Mike Bobsin <mikefotografias@gmail.com>
2021-04-22 21:08:07 -0300

    Setup bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -129,18 +129,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css");
 h3 {
     margin: 40px 0 0;
 }
 ul {
     list-style-type: none;
     padding: 0;
-}
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
+    li {
+        display: inline-block;
+        margin: 0 10px;
+        a {
+            color: #42b983;
+        }
+    }
 }
 </style>
