b93b42bca13c037d9c5fc97d2d2dd2e0ea036ced
Mike Bobsin <mikefotografias@gmail.com>
2021-04-23 02:13:09 -0300

    Config bootstrap and routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 import router from "./router";
 import store from "./store";
 
 createApp(App).use(store).use(router).mount("#app");
+
+//for vue dev tools only
+Vue.config.devtools = true;
